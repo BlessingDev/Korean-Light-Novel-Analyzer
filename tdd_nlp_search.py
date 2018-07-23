a197beade897b47655b7df33b9c935d01f55da15
BLESSINGDEV-LAP\wnsgh <wnsghekd2006@naver.com>
2018-07-23 11:53:55 +0900

    tdd

--- tdd_nlp_search.py
+++ tdd_nlp_search.py
@@ -1,22 +1,29 @@
 import unittest
 from BookData import BookData
-from NaverBookSearcher import NaverBookSearcher
+from external_tools import NaverBookSearcher as nb
 import nlp_module
 
 class NaverSearchTest(unittest.TestCase) :
 
     def test_normal_set1(self) :
         book = BookData()
-        sc = NaverBookSearcher()
+        sc = nb.NaverBookSearcher()
         sc.book = book
         sc.from_title('바보와 시험과 소환수 10')
         self.assertEqual("바보와 시험과 소환수 10", book.title)
 
     def test_extraordinary_set1(self) :
         book = BookData()
-        sc = NaverBookSearcher()
+        sc = nb.NaverBookSearcher()
         sc.book = book
         sc.from_title('시큐브 11')
+        self.assertEqual("C3 시큐브 11 (Extreme Novel)", book.title)
+
+    def test_extraordinary_set2(self) :
+        book = BookData()
+        sc = nb.NaverBookSearcher()
+        sc.book = book
+        sc.from_title('너 또한, 위장연인(오타쿠)이라 할지라도 1 上')
         self.assertEqual("C3 시큐브 11 (Extreme Novel)", book.title)
 
 class NLPTest(unittest.TestCase) :
